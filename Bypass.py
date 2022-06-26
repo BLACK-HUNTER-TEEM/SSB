7c29c869ee34390517d5c92eb1a0ca0236db253a
BLACK-HUNTER-TEEM <105848361+BLACK-HUNTER-TEEM@users.noreply.github.com>
2022-06-27 00:25:43 +0600

    Update Bypass.py

--- Bypass.py
+++ Bypass.py
@@ -3,7 +3,7 @@ import os, platform, time
 b = platform.architecture()[0]
 
 if b == '64bit':
-    print("  \x1b[1;97m\x1b[1;41m Fuck SSB - Fuck Your Next Update Soon... \x1b[0m")
+    print("  \x1b[1;97m\x1b[1;41m Fuck SSB - BLACK HUNTER TEAM \x1b[0m")
 
     print("\n\x1b[1;92mCongratulations Your Device Support This Tool\033[1;37m")
     print("\n\x1b[1;92m         Toll Loading Please Wait... ") 
